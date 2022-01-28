c218f80452056be7fbb1a148112f496b58d71b5b
Sarthak Verma <sarthakvdev@gmail.com>
2022-01-29 01:44:16 +0530

    converted back to js

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return(
+    <footer className="flex h-24 w-full items-center justify-center border-t">
+      <p className="text-center text-gray-700">Created by Sarthak Verma</p>   
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>e-commerce</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      </main>
+    </div>
+  )
+}
